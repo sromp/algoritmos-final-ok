6996fcdec6651bad644caf2a14838c9c8d85178e
sromp <spentofficial@gmail.com>
2021-11-21 20:09:32 -0600

    Creado nuevo repositorio

---
+++ Codigo/VisualStudio/AlgoritmosFinalOK/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace AlgoritmosFinalOK
+{
+	class Program
+	{
+		struct DatosPelicula
+		{
+			public string nombre;
+			public double costoAdulto;
+			public double costoMenores;
+			public int boletosVendidosMenores;
+			public int boletosVendidosAdulto;
+			public double dineroDescontado;
+
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+		}
+
+
+		// 
+		static void EscribeYLeeInt(string textoPedir, string textoError, out int resultado, int resultadoMayorIgualQue, int resultadoMenorIgualQue)
+		{
+			bool continuar = true;
+
+			do
+			{
+				Console.WriteLine(textoPedir);
+				if (int.TryParse(Console.ReadLine(), out resultado) && resultado >= resultadoMayorIgualQue && resultado <= resultadoMenorIgualQue)
+					continuar = false;
+				else
+					Console.WriteLine(textoError);
+			} while (continuar);
+		}
+
+		static void EscribeYLeeDouble(string textoPedir, string textoError, out double resultado, double resultadoMayorIgualQue, double resultadoMenorIgualQue)
+		{
+			bool continuar = true;
+
+			do
+			{
+				Console.WriteLine(textoPedir);
+				if (double.TryParse(Console.ReadLine(), out resultado) && resultado >= resultadoMayorIgualQue && resultado <= resultadoMenorIgualQue)
+					continuar = false;
+				else
+					Console.WriteLine(textoError);
+			} while (continuar);
+		}
+	}
+}
